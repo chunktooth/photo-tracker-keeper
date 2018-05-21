fb0ea479ffa2f02682fc370bcd92ea20abfd8208
chunktooth <om.saetehn@gmail.com>
2018-05-21 11:05:38 -0600

    GET POST DELETE endpoints

--- server.js
+++ server.js
@@ -12,16 +12,44 @@ app.locals.title = 'Photo Tracker Keeper';
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-// app.get('/', (request, response) => {
-//   response.send('Oh yeah it\'s on!');
-// });
 
 app.get('/api/v1/photos', (request, response) => {
   database('photos').select()
   .then(photo => {
-    response.status(200).json
+    response.status(200).json(photo);
   })
-})
+  .catch(error => {
+    response.status(404).json(error);
+  });
+});
+
+app.post('/api/v1/photos', (request, response) => {
+  const photo = request.body;
+
+  if(!photo.name || !photo.url) {
+    return response.status(422).send({ error: 'Missing data'})
+  } else {
+    database('photos').insert(photo, 'id')
+    .then(photo => {
+      response.status(201).json({ id: photo[0] })
+    })
+    .catch(error => {
+      response.status(500).json(error);
+    });
+  };
+});
+
+app.delete('/api/v1/photos/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('photos').where('id', id).delete()
+  .then(photo => {
+    response.status(202).json(`${photo} deleted!`);
+  })
+  .catch(error => {
+    response.status(500).json(error);
+  });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
