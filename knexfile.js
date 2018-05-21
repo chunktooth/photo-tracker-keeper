0cb603d30547c4cb35642ae9e29044475c21c1f1
chunktooth <om.saetehn@gmail.com>
2018-05-21 10:49:28 -0600

    ran seed

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,36 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/fotokeeper',
     migrations: {
       directory: './db/fotokeeper'
     },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
     userNullAsDefault: true
-  }
+  },
 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/fotokeeper_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+  
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 };
--- server.js
+++ server.js
@@ -12,9 +12,16 @@ app.locals.title = 'Photo Tracker Keeper';
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/', (request, response) => {
-  response.send('Oh yeah it\'s on!');
-});
+// app.get('/', (request, response) => {
+//   response.send('Oh yeah it\'s on!');
+// });
+
+app.get('/api/v1/photos', (request, response) => {
+  database('photos').select()
+  .then(photo => {
+    response.status(200).json
+  })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
