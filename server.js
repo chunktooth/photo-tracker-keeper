f5f2d55a1570155c6c45196141f7c25e3b2e9f5d
chunktooth <om.saetehn@gmail.com>
2018-05-21 09:54:36 -0600

    set up static directory

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Photo Tracker Keeper';
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
   response.send('Oh yeah it\'s on!');
@@ -11,3 +19,5 @@ app.get('/', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = app;
