c3db533ddf251ec6a897753969ac15a8149a424f
chunktooth <om.saetehn@gmail.com>
2018-05-21 11:48:11 -0600

    styling photo tile and POST photo to server

--- public/script.js
+++ public/script.js
@@ -11,9 +11,40 @@ function loadPhotos(photos) {
   .then(response => response.json())
   .then(photos => photos.forEach(photo => {
     return $('.display').append(`
-      <h1 class="name">${photo.name}</h1>
-      <img src=${photo.url} class="photo" />
+      <div class="img-tile">
+        <h1 class="name">${photo.name}</h1>
+        <img src=${photo.url} class="photo" />
+      </div>
     `)
   }))
   .catch(error => console.log(error));
-}
+};
+
+$('.add-btn').on('click', addPhoto);
+
+function addPhoto(event) {
+  event.preventDefault();
+  let titleInput = $('.title-input').val();
+  let urlInput = $('.url-input').val();
+
+  return fetch('/api/v1/photos', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: titleInput,
+      url: urlInput
+    }),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(photo => console.log(photo))
+  .catch(error => console.log(error));
+
+  resetForm();
+};
+
+function resetForm() {
+  $('.title-input').val('');
+  $('.url-input').val('');
+};
