91c04145d93d285b7146ec18495d7d6e16e1bd46
chunktooth <om.saetehn@gmail.com>
2018-05-22 13:24:40 -0600

    no reloading required

--- public/script.js
+++ public/script.js
@@ -40,7 +40,7 @@ function addPhoto(event) {
   })
   .then(response => response.json())
   .then(photo => console.log(photo))
-  .then(() => location.reload())
+  .then(() => loadPhotos())
   .catch(error => console.log(error));
   resetForm();
 };
@@ -56,7 +56,7 @@ function deletePhoto() {
   return fetch(`/api/v1/photos/${id}`, {
     method: 'DELETE'
   })
-  .then(() => location.reload())
+  .then(() => loadPhotos())
   .catch(error => console.log(error));
   $(this).parent().remove();
 }
